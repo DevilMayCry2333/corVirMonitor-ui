c91c55e59666c51a323564fb799d033d3c3cd8d4
ykaiyu <1712363499@qq.com>
2020-02-24 00:41:53 +0800

    aaaa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeyComponent } from './hey/hey.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'hey',component:HeyComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/hey/hey.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hey',
+  templateUrl: './hey.component.html',
+  styleUrls: ['./hey.component.scss']
+})
+export class HeyComponent {
+  title = 'hey';
+}
