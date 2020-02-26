1b5872e5f67703bab3d6cb6baf776aad0f3c4165
ykaiyu <1712363499@qq.com>
2020-02-26 21:50:10 +0800

    1111

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,13 @@ import { Component, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  cityName: string;
+  currentConfirmedCount: number;
+  confirmedCount: number;
+  curedCount: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
 ];
 
 @Component({
@@ -30,12 +20,20 @@ const ELEMENT_DATA: PeriodicElement[] = [
 @Injectable()
 export class AppComponent {
   title = 'hello';
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['cityName', 'currentConfirmedCount', 'confirmedCount', 'curedCount'];
+  dataSource=[];
+
   constructor(private http: HttpClient){
 
   }
   ngOnInit(){
     console.log("hello");
+    this.http.get("https://youkaiyu.com:8443/").subscribe((res:PeriodicElement)=>{
+      ELEMENT_DATA.push(res);
+      this.dataSource = ELEMENT_DATA;
+      console.log(this.dataSource);
+      console.log(res);
+    })
   }
+
 }
